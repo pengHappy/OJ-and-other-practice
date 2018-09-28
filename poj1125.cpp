b9a8f963b0b57f70f5ef9756ca9359eee27259bc
pzha <pzha@amazon.com>
2018-09-27 17:18:31 -0700

    poj1125 result is longest path among all the shortest path

--- poj1125.cpp
+++ poj1125.cpp
@@ -20,6 +20,7 @@ const int MAX_N = 100 + 1;
 const int MAX_INT = 1e9;
 int dp[2][MAX_N][MAX_N];
 int n, contacts, from, to, timeCost;
+int person, longestTime;
 
 int main() {
     while(scanf("%d", &n) != EOF && n) {
@@ -43,6 +44,10 @@ int main() {
                     dp[k % 2][i][j] = min(dp[(k - 1) % 2][i][j], dp[(k - 1) % 2][i][k] + dp[(k - 1) % 2][k][j]);
                 }
             }
+        }
+        longestTime = -1;
+        for(int i = 1; i <= n; i++) {
+            
         }
     }
     return 0;
