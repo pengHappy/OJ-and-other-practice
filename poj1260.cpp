a2b3bb4c4e47332f9d97c3be300c6f2e11b75bf1
pzha <pzha@amazon.com>
2018-09-28 17:27:14 -0700

    poj1260 Pearls dp

---
+++ poj1260.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <map>
+#include <set>
+#include <stack>
+#include <functional>
+
+using namespace std;
+
+const int MAX_C = 100 + 1;
+const int MAX_INT = 1e9;
+
+int t, c, amount[MAX_C], price[MAX_C];
+
+int main() {
+    scanf("%d", &t);
+    while(t--) {
+        scanf("%d", &c);
+    }
+    return 0;
+}
